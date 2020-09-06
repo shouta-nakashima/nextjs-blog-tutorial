24fd30f825567900a59494b48e2eb86fac901145
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-09-06 12:09:22 +0900

    changed-typeScript

--- lib/posts.ts
+++ lib/posts.ts
@@ -26,7 +26,7 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data
+      ...(matterResult.data as { date: string; title: string})
     }
   })
   // Sort posts by date
@@ -90,6 +90,6 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data
+    ...(matterResult.data as { date: string; title: string})
   }
 }
